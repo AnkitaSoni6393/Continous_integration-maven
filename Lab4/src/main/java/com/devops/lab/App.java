711d0f93545b40999448ad6cd27663e9cf2b3c8c
AnkitaSoni6393 <droankita@gmail.com>
2025-07-14 17:00:59 +0530

    Add files via upload

--- Lab4/src/main/java/com/devops/lab/App.java
+++ Lab4/src/main/java/com/devops/lab/App.java
@@ -1,1 +1,13 @@
+package com.devops.lab;
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
